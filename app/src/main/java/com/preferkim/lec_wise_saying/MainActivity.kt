6f84379baaf696e49c5f420e20c22bb27b2ff8aa
Balparang <shineecard5@gmail.com>
2022-03-28 15:34:29 +0900

    [추가] 코틀린 리스트 사용 방법

--- app/src/main/java/com/preferkim/lec_wise_saying/MainActivity.kt
+++ app/src/main/java/com/preferkim/lec_wise_saying/MainActivity.kt
@@ -2,6 +2,7 @@ package com.preferkim.lec_wise_saying
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,6 +10,18 @@ class MainActivity : AppCompatActivity() {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        // 코틀린 리스트 사용법
+        val testList = mutableListOf<String>()
+        testList.add("a")
+        testList.add("b")
+        testList.add("c")
+
+        Log.d("MainActivity", testList.toString())
+
+        Log.d("MainActivity", testList[1])
+
 
     }
 
